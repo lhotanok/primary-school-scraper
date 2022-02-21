75ffb7e9ae907bcbd07c257c7a0914a5605eb053
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2022-02-21 23:53:35 +0100

    Move helper functions from `routes.js` to `tools.js`

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const Apify = require('apify');
-const { SELECTORS, LABELS, RESULTS_PER_PAGE, OFFSET_QUERY_PARAMETER } = require('./constants');
+const { enqueueListPage, enqueuePaginationPage, enqueueDetailPages } = require('./tools');
+const { SELECTORS } = require('./constants');
 
 const { utils: { log } } = Apify;
 
@@ -32,57 +33,4 @@ exports.handleList = async (context) => {
 
 exports.handleDetail = async ({ request, $ }) => {
     // Handle details
-};
-
-const enqueueListPage = async (url, requestQueue, nextPage) => {
-    const request = {
-        url,
-        userData: {
-            label: LABELS.LIST,
-            currentPage: nextPage,
-        },
-    };
-
-    await requestQueue.addRequest(request);
-    log.debug(`Enqueued request: ${JSON.stringify(request, null, 2)}`);
-};
-
-const enqueuePaginationPage = async ({ $, request, crawler: { requestQueue } }) => {
-    const { userData: { currentPage } } = request;
-
-    const nextUrl = buildNextPageUrl(request);
-    const nextOffset = parseInt(nextUrl.searchParams.get(OFFSET_QUERY_PARAMETER), 10);
-    const totalSchools = parseInt($(SELECTORS.TOTAL_SCHOOLS).text().trim(), 10);
-
-    if (nextOffset <= totalSchools) {
-        await enqueueListPage(nextUrl.toString(), requestQueue, currentPage + 1);
-    }
-};
-
-const enqueueDetailPages = async ({ $, crawler: { requestQueue } }) => {
-    const schoolElements = $(SELECTORS.SCHOOLS);
-
-    const schoolUrls = schoolElements.map((_i, el) => $(el).attr('href')).toArray();
-    log.debug(`Extracted ${schoolUrls.length} school urls`);
-
-    for (const url of schoolUrls) {
-        const request = {
-            url,
-            userData: {
-                label: LABELS.DETAIL,
-            },
-        };
-        await requestQueue.addRequest(request);
-    }
-};
-
-const buildNextPageUrl = (currentRequest) => {
-    const { url, userData: { currentPage } } = currentRequest;
-
-    const nextUrl = new URL(url);
-    const nextPage = currentPage + 1;
-    const nextOffset = nextPage * RESULTS_PER_PAGE;
-
-    nextUrl.searchParams.set(OFFSET_QUERY_PARAMETER, nextOffset);
-    return nextUrl;
 };
--- src/tools.js
+++ src/tools.js
@@ -1,6 +1,9 @@
 const Apify = require('apify');
+const { SELECTORS, LABELS, RESULTS_PER_PAGE, OFFSET_QUERY_PARAMETER } = require('./constants');
 
-exports.initializeRequestQueue = async (startRegions) => {
+const { utils: { log } } = Apify;
+
+const initializeRequestQueue = async (startRegions) => {
     const requestQueue = await Apify.openRequestQueue();
 
     for (const region of startRegions) {
@@ -9,4 +12,64 @@ exports.initializeRequestQueue = async (startRegions) => {
     }
 
     return requestQueue;
+};
+
+const enqueueListPage = async (url, requestQueue, nextPage) => {
+    const request = {
+        url,
+        userData: {
+            label: LABELS.LIST,
+            currentPage: nextPage,
+        },
+    };
+
+    await requestQueue.addRequest(request);
+    log.debug(`Enqueued request: ${JSON.stringify(request, null, 2)}`);
+};
+
+const enqueuePaginationPage = async ({ $, request, crawler: { requestQueue } }) => {
+    const { userData: { currentPage } } = request;
+
+    const nextUrl = buildNextPageUrl(request);
+    const nextOffset = parseInt(nextUrl.searchParams.get(OFFSET_QUERY_PARAMETER), 10);
+    const totalSchools = parseInt($(SELECTORS.TOTAL_SCHOOLS).text().trim(), 10);
+
+    if (nextOffset <= totalSchools) {
+        await enqueueListPage(nextUrl.toString(), requestQueue, currentPage + 1);
+    }
+};
+
+const enqueueDetailPages = async ({ $, crawler: { requestQueue } }) => {
+    const schoolElements = $(SELECTORS.SCHOOLS);
+
+    const schoolUrls = schoolElements.map((_i, el) => $(el).attr('href')).toArray();
+    log.debug(`Extracted ${schoolUrls.length} school urls`);
+
+    for (const url of schoolUrls) {
+        const request = {
+            url,
+            userData: {
+                label: LABELS.DETAIL,
+            },
+        };
+        await requestQueue.addRequest(request);
+    }
+};
+
+const buildNextPageUrl = (currentRequest) => {
+    const { url, userData: { currentPage } } = currentRequest;
+
+    const nextUrl = new URL(url);
+    const nextPage = currentPage + 1;
+    const nextOffset = nextPage * RESULTS_PER_PAGE;
+
+    nextUrl.searchParams.set(OFFSET_QUERY_PARAMETER, nextOffset);
+    return nextUrl;
+};
+
+exports = {
+    initializeRequestQueue,
+    enqueueListPage,
+    enqueuePaginationPage,
+    enqueueDetailPages,
 };
