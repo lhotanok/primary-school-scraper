b999a12b45391204db37692a58f1007adb45e896
Michael Bohin <mbohin@seznam.cz>
2022-02-24 12:38:55 +0100

    commit "update"

--- results-merge-manager.js
+++ results-merge-manager.js
@@ -14,18 +14,7 @@ function getDomain(url) {
     return domain.get(url);
 }
 
-function main() {
-    const SCHOOLS_RESULT_PATH = 'schools-scraper/apify_storage/key_value_stores/default/SCHOOLS_RESULT.json';
-    const DOMAINS_RESULT_PATH = 'contacts-scraper/apify_storage/key_value_stores/default/DOMAINS_RESULT.json';
-    const CHEERIO_DOMAINS_RESULT_PATH = 'cheerio-contacts-scraper/apify_storage/key_value_stores/default/DOMAINS_RESULT.json';
-
-    const RESULTS_PATH = 'RESULTS.json';
-    const CHEERIO_RESULTS_PATH = 'CHEERIO_RESULTS.json';
-
-    const schoolsResult = JSON.parse(readFile(SCHOOLS_RESULT_PATH));
-    const domainsResult = JSON.parse(readFile(DOMAINS_RESULT_PATH));
-    const cheerioDomainsResult = JSON.parse(readFile(CHEERIO_DOMAINS_RESULT_PATH));
-
+function mergeResults(schoolResults, domainsResult) {
     const results = schoolsResult.map((schoolResult) => {
         const result = schoolResult;
 
@@ -37,24 +26,33 @@ function main() {
                 result.emails = emails;
                 result.erasmusList = erasmusList;
             }
-
-            if (cheerioDomainsResult[domain]) {
-                const { emails, erasmusList } = cheerioDomainsResult[domain];
-                result.emails = emails;
-                result.erasmusList = erasmusList;
-            }
-
-            result.emails = Array.from(new Set(result.emails))
-            result.erasmusList = Array.from(new Set(result.erasmusList))
         }
 
         return result;
     });
 
+    return results;
+}
+
+function main() {
+    const SCHOOLS_RESULT_PATH = 'schools-scraper/apify_storage/key_value_stores/default/SCHOOLS_RESULT.json';
+    const PUPPETEER_DOMAINS_RESULT_PATH = 'contacts-scraper/apify_storage/key_value_stores/default/DOMAINS_RESULT.json';
+    const CHEERIO_DOMAINS_RESULT_PATH = 'cheerio-contacts-scraper/apify_storage/key_value_stores/default/DOMAINS_RESULT.json';
+
+    const PUPPETEER_RESULTS_PATH = 'PUPPETEER_RESULTS.json';
+    const CHEERIO_RESULTS_PATH = 'CHEERIO_RESULTS.json';
+
+    const schoolsResult = JSON.parse(readFile(SCHOOLS_RESULT_PATH));
+    const puppeteerDomainsResult = JSON.parse(readFile(PUPPETEER_DOMAINS_RESULT_PATH));
+    const cheerioDomainsResult = JSON.parse(readFile(CHEERIO_DOMAINS_RESULT_PATH));
+
+    const puppeteerMergedResults = mergeResults(schoolsResult, puppeteerDomainsResult);
+    const cheerioMergedResults = mergeResults(schoolsResult, cheerioDomainsResult);
+
     console.log(`Merged ${results.length} school results`);
 
-    writeFile(RESULTS_PATH, JSON.stringify(results, null, 2));
-    writeFile(CHEERIO_RESULTS_PATH, JSON.stringify(results, null, 2));
+    writeFile(PUPPETEER_RESULTS_PATH, JSON.stringify(puppeteerMergedResults, null, 2));
+    writeFile(CHEERIO_RESULTS_PATH, JSON.stringify(cheerioMergedResults, null, 2));
 }
 
 main()
